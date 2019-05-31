c090fbf7cfd4fda65658f5edf0d87eb236289345
Andrew Keele <hiimandyk@MacBook-Pro-2.local>
2019-05-30 18:37:25 -0600

    inital commitment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './reset.css';
+import background from './images/background.jpg';
+import tooth from './images/tooth.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="background-image">
+
+        <img src={background} className="background-photo" alt="background" />
+        <div className="header">
+          <div className="header-text">
+            DownEast Dental
+        </div>
+      </div>
+        </div>
+        <div className="tooth-image">
+          <img src={tooth} className="tooth-photo" alt="tooth"></img>
+        </div>
+
+
+
+      <div className="footer">
+        <div className="footer-text-one">
+          Let Me Stick 
+
+          </div>
+          <div className="footer-text-two">
+          My Drill In Your Mouth
+          </div>
+      </div>
     </div>
   );
 }
