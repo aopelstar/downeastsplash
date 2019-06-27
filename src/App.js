a743b591bc722f3e734a3a35999bf36e1ffb7ef7
Andrew Keele <hiimandyk@MacBook-Pro-2.local>
2019-06-27 00:14:55 -0600

    under construction stuff

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
 import React from 'react';
 import './App.css';
+import './index.css'
 import './reset.css';
 import background from './images/background.jpg';
-import tooth from './images/tooth.png'
+import tooth from './images/logo.png'
 
 function App() {
   return (
     <div className="App">
-      <div className="background-image">
+      <img src={background} className="background-image" alt="whatevs" />
+      <div className="header">
+      <img src={tooth} className="logo-image" alt="whatevs"/>
 
-        <img src={background} className="background-photo" alt="background" />
-        <div className="header">
-          <div className="header-text">
-            DownEast Dental
-        </div>
       </div>
-        </div>
-        <div className="tooth-image">
-          <img src={tooth} className="tooth-photo" alt="tooth"></img>
-        </div>
-
-
-
       <div className="footer">
-        <div className="footer-text-one">
-          Let Me Stick 
-
-          </div>
-          <div className="footer-text-two">
-          My Drill In Your Mouth
-          </div>
+        <div>
+          New website coming soon
+        </div>
+        <div>
+          Now accepting new patients.
+        </div>
+        <div>
+          Please call: <a href= "tel:207-667-3009">(207) 667-3009</a> to make an appointment.
+        </div>
       </div>
     </div>
   );
